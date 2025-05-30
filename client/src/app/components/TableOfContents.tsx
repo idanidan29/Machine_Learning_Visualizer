e5ab01bd10ba84a6d0450da2609655979d69448e
Idan Levi <29idan29@gmail.com>
2025-05-30 19:09:02 +0300

    small bug fix

--- client/src/app/components/TableOfContents.tsx
+++ client/src/app/components/TableOfContents.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { motion, AnimatePresence } from 'framer-motion';
-import { algorithms, type Algorithm } from '../data/algorithms';
+import { algorithms } from '../data/algorithms';
 
 interface Section {
   id: string;
