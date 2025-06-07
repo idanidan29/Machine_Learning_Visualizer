e6dd18011cb0c3758cb8043354ae6085b7705549
Idan Levi <29idan29@gmail.com>
2025-06-08 00:20:05 +0300

    migrating the .Net app to Docker container

--- Api/Program.cs
+++ Api/Program.cs
@@ -81,12 +81,10 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
+app.UseSwagger();
+app.UseSwaggerUI();
+
+if (!app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
 }
